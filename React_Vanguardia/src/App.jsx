a70175560af687e6896ec9b5b57efb4b657dc46a
EdgarMembreno <edgar.membreno2014.com>
2024-06-05 19:50:55 -0600

    Primer avance de react
    Primer avance de react

---
+++ React_Vanguardia/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React con Vite</h1>
+        <p>
+          Prueba de la creacion de react .
+        </p>
+        <button className="primary-button">Comenzar</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
